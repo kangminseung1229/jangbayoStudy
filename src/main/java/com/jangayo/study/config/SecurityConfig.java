26527c81268067190b75499f363f4c84758d4096
kangminseung1229 <73506074+kangminseung1229@users.noreply.github.com>
2022-02-18 23:14:13 +0900

    중간점검테스트

--- src/main/java/com/jangayo/study/config/SecurityConfig.java
+++ src/main/java/com/jangayo/study/config/SecurityConfig.java
@@ -1,36 +1,29 @@
 package com.jangayo.study.config;
-
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/*
-spring security 설정
-*/
 @Configuration
-@EnableWebSecurity(debug = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity()
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // TODO Auto-generated method stub
 
-        http.authorizeHttpRequests()
-                .mvcMatchers("/").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests()
+            .mvcMatchers("/security/test1").permitAll()
+            .anyRequest().authenticated();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         // TODO Auto-generated method stub
-        // static folder는 시큐리티 체인에 해당되지 않는다.
+
         web.ignoring()
-                .mvcMatchers("/node_modules/**")
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-
+            .mvcMatchers("/css/**")
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
-
+    
 }
