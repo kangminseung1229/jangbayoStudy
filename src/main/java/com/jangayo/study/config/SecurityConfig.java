ab464019aa0a2419a2e180e9a5b7c8e495492376
kangminseung1229 <73506074+kangminseung1229@users.noreply.github.com>
2022-03-15 23:59:33 +0900

    rememberME 진행중

--- src/main/java/com/jangayo/study/config/SecurityConfig.java
+++ src/main/java/com/jangayo/study/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package com.jangayo.study.config;
-import org.hibernate.type.TrueFalseType;
+import javax.sql.DataSource;
+
+import com.jangayo.study.account.AccountService;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +39,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         http.logout()
             .logoutSuccessUrl("/");
 
+        //remember me
+        http.rememberMe()
+            .userDetailsService(accountService)
+            .tokenRepository(tokenRepository());
+
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl(); // 해당 상위 클래스의 쿼리문에 해당하는 클래스를 생성해주어야 한다.
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+
+        System.out.println("========================쿠키생성========================");
+        System.out.println(jdbcTokenRepositoryImpl);
+        System.out.println("========================쿠키생성========================");
+
+        return jdbcTokenRepositoryImpl;
     }
 
     @Override
